6a3fb7f79332ab667d5b12b77534893a54397fb6
Sachin Philip Mathew <me@imsach.in>
2016-06-27 15:31:19 +0530

    complete change

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#hero").height($(window).height());
-    $(window).resize(function() {
-        $("#hero").height($(window).height());
-        $("#hero").css("min-height", "600px")
-    });
-
-});
