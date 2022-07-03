fb310ef3d7ca4cfdb4de9ec15c53de84bce6d0f1
Sanjana - Sachin <labtocat@Sachins-MacBook-Pro.fritz.box>
2022-07-03 21:23:46 +0200

    Svelte wrapper

---
+++ profile-v2/src/routes/index.svelte
@@ -0,0 +1,82 @@
+<div class="grid place-items-center h-screen">
+	<div class="container">
+		<p class="text-4xl font-bold">Hello from Sachin 👋🏽</p>
+		<div class="text-slate-500 dark:text-slate-400">
+			lives in 📍
+			<a target="_blank" href="https://en.wikipedia.org/wiki/Munich" rel="noreferrer">Munich</a>
+			🇩🇪 from Kottayam,
+			<a target="_blank" href="https://en.wikipedia.org/wiki/Kerala" rel="noreferrer">Kerala</a>
+			🇮🇳
+		</div>
+		<br />
+		<div class="text-slate-600 dark:text-slate-300 mt-2 text-lg font-light">
+			I'm a husband, developer advocate and polyglot engineer working with anything I can get my
+			hands on, with a specific taste towards web 🌎. My aim is to build experiences that make
+			people's lives simpler. Having worked with Javascript and Python eco-system, I have currently
+			moved on to explore the green pastures of
+			<a target="_blank" href="https://deno.land/" rel="noreferrer">deno</a>
+			and
+			<a target="_blank" href="https://www.rust-lang.org/" rel="noreferrer">rust</a>
+			🤓.
+			<br />
+			Technology aside, I am a dog person these days 🐕, an interest that took seed during the
+			pandemic of 2020. Love spending time with books,
+			<a target="_blank" href="https://g.co/kgs/4g13UG" rel="noreferrer">Zero to One</a>
+			being my current favourite.
+			<br />
+			<br />
+			<div class="columns column is-9">
+				<ul class="is-size-6">
+					<li class="mb-1">
+						<i class="fas fa-angle-right" />
+						👨🏽‍💻 I work 🏡 with
+						<a target="_blank" href="https://www.everreal.co/" rel="noreferrer">Everreal Gmbh</a>
+						focused on digital solution for renting and selling in 🇩🇪 and 🇨🇭 soon will be in 🇦🇹.
+					</li>
+					<li class="mb-1">
+						<i class="fas fa-angle-right" />
+						🏗 Find more about my works on
+						<a target="_blank" href="https://gitlab.com/sachin-philip" rel="noreferrer">
+							gitlab
+							<i class="fab fa-gitlab" />
+						</a>
+						and
+						<a target="_blank" href="https://github.com/sachin-philip" rel="noreferrer">
+							github
+							<i class="fab fa-github" />
+						</a>
+						.
+					</li>
+					<li class="mb-1">
+						<i class="fas fa-angle-right" />
+						📬 Reach me on
+						<a target="_blank" href="https://twitter.com/sachin_philip" rel="noreferrer">
+							twitter
+							<i class="fab fa-twitter-square" />
+						</a>
+						,
+						<a target="_blank" href="https://www.linkedin.com/in/sachinphilip/" rel="noreferrer">
+							linkedin
+							<i class="fab fa-linkedin" />
+						</a>
+						or
+						<a target="_blank" href="mailto:me@sachinphilip.com" rel="noreferrer">
+							mail me
+							<i class="fas fa-envelope" />
+						</a>
+						.
+					</li>
+				</ul>
+			</div>
+		</div>
+		<br />
+		<br />
+		<p>
+			code with
+			<i
+				class="fas fa-heart animate__animated animate__pulse animate__infinite infinite"
+				style="color: #f14668" />
+		</p>
+
+	</div>
+</div>
