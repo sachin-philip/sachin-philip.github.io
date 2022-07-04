345eb57eed4c81f0e1fe967b1ad5ea4791c8dc82
Sachin Philip Mathew <sachinvettithanam@gmail.com>
2022-07-04 18:16:50 +0200

    Updates

---
+++ _app/immutable/pages/index.svelte-f3d99afb.js
@@ -0,0 +1,85 @@
+import{S as ge,i as pe,s as me,e as r,t as s,k as m,c as a,a as l,h as o,d as n,m as v,b as e,f as ve,g as be,I as t,E as ne}from"../chunks/index-d52c1a7c.js";function _e(he){let x,c,G,et,rt,b,at,A,nt,lt,R,st,ot,it,ct,dt,i,ut,L,ft,ht,B,gt,pt,mt,vt,D,bt,_t,wt,kt,Et,It,q,f,K,T,yt,M,xt,At,Rt,h,U,Lt,_,Bt,Z,Dt,w,Kt,C,Mt,Vt,g,j,Ft,k,Ht,z,Pt,E,St,N,Gt,qt,p,Q,Jt,I,Ot,W,Tt,y,Ut,X,Zt,Ct,jt,zt,Nt,Qt,F,Wt,J;return{c(){x=r("div"),c=r("div"),G=r("p"),et=s("Hello from Sachin \u{1F44B}\u{1F3FD}"),rt=m(),b=r("div"),at=s(`lives in \u{1F4CD}
+			`),A=r("a"),nt=s("Munich"),lt=s(`
+			\u{1F1E9}\u{1F1EA} from Kottayam,
+			`),R=r("a"),st=s("Kerala"),ot=s(`
+			\u{1F1EE}\u{1F1F3}`),it=m(),ct=r("br"),dt=m(),i=r("div"),ut=s(`I'm a husband, developer advocate and polyglot engineer working with anything I can get my
+			hands on, with a specific taste towards web \u{1F30E}. My aim is to build experiences that make
+			people's lives simpler. Having worked with Javascript and Python eco-system, I have currently
+			moved on to explore the green pastures of
+			`),L=r("a"),ft=s("deno"),ht=s(`
+			and
+			`),B=r("a"),gt=s("rust"),pt=s(`
+			\u{1F913}.
+			`),mt=r("br"),vt=s(`
+			Technology aside, I am a dog person these days \u{1F415}, an interest that took seed during the
+			pandemic of 2020. Love spending time with books,
+			`),D=r("a"),bt=s("Zero to One"),_t=s(`
+			being my current favourite.
+			`),wt=r("br"),kt=m(),Et=r("br"),It=m(),q=r("div"),f=r("ul"),K=r("li"),T=r("i"),yt=s(`
+						\u{1F468}\u{1F3FD}\u200D\u{1F4BB} I work with
+						`),M=r("a"),xt=s("Everreal Gmbh"),At=s(`
+						focused on digital solution for renting and selling in \u{1F1E9}\u{1F1EA} and \u{1F1E8}\u{1F1ED} soon will be in \u{1F1E6}\u{1F1F9}.`),Rt=m(),h=r("li"),U=r("i"),Lt=s(`
+						\u{1F3D7} Find more about my works on
+						`),_=r("a"),Bt=s(`gitlab
+							`),Z=r("i"),Dt=s(`
+						and
+						`),w=r("a"),Kt=s(`github
+							`),C=r("i"),Mt=s(`
+						.`),Vt=m(),g=r("li"),j=r("i"),Ft=s(`
+						\u{1F4EC} Reach me on
+						`),k=r("a"),Ht=s(`linkedin
+							`),z=r("i"),Pt=s(`
+						or
+						`),E=r("a"),St=s(`mail me
+							`),N=r("i"),Gt=s(`
+						.`),qt=m(),p=r("li"),Q=r("i"),Jt=s(`
+						\u{1FA82} Follow me on
+						`),I=r("a"),Ot=s(`twitter
+							`),W=r("i"),Tt=s(`
+						and
+						`),y=r("a"),Ut=s(`500px
+							`),X=r("i"),Zt=s(`
+						.`),Ct=m(),jt=r("br"),zt=m(),Nt=r("br"),Qt=m(),F=r("p"),Wt=s(`crafted with
+			`),J=r("i"),this.h()},l(Y){x=a(Y,"DIV",{class:!0});var tt=l(x);c=a(tt,"DIV",{class:!0});var u=l(c);G=a(u,"P",{class:!0});var le=l(G);et=o(le,"Hello from Sachin \u{1F44B}\u{1F3FD}"),le.forEach(n),rt=v(u),b=a(u,"DIV",{class:!0});var O=l(b);at=o(O,`lives in \u{1F4CD}
+			`),A=a(O,"A",{class:!0,target:!0,href:!0,rel:!0});var se=l(A);nt=o(se,"Munich"),se.forEach(n),lt=o(O,`
+			\u{1F1E9}\u{1F1EA} from Kottayam,
+			`),R=a(O,"A",{class:!0,target:!0,href:!0,rel:!0});var oe=l(R);st=o(oe,"Kerala"),oe.forEach(n),ot=o(O,`
+			\u{1F1EE}\u{1F1F3}`),O.forEach(n),it=v(u),ct=a(u,"BR",{}),dt=v(u),i=a(u,"DIV",{class:!0});var d=l(i);ut=o(d,`I'm a husband, developer advocate and polyglot engineer working with anything I can get my
+			hands on, with a specific taste towards web \u{1F30E}. My aim is to build experiences that make
+			people's lives simpler. Having worked with Javascript and Python eco-system, I have currently
+			moved on to explore the green pastures of
+			`),L=a(d,"A",{class:!0,target:!0,href:!0,rel:!0});var ie=l(L);ft=o(ie,"deno"),ie.forEach(n),ht=o(d,`
+			and
+			`),B=a(d,"A",{class:!0,target:!0,href:!0,rel:!0});var ce=l(B);gt=o(ce,"rust"),ce.forEach(n),pt=o(d,`
+			\u{1F913}.
+			`),mt=a(d,"BR",{}),vt=o(d,`
+			Technology aside, I am a dog person these days \u{1F415}, an interest that took seed during the
+			pandemic of 2020. Love spending time with books,
+			`),D=a(d,"A",{class:!0,target:!0,href:!0,rel:!0});var de=l(D);bt=o(de,"Zero to One"),de.forEach(n),_t=o(d,`
+			being my current favourite.
+			`),wt=a(d,"BR",{}),kt=v(d),Et=a(d,"BR",{}),It=v(d),q=a(d,"DIV",{class:!0});var ue=l(q);f=a(ue,"UL",{class:!0});var V=l(f);K=a(V,"LI",{class:!0});var $=l(K);T=a($,"I",{class:!0}),l(T).forEach(n),yt=o($,`
+						\u{1F468}\u{1F3FD}\u200D\u{1F4BB} I work with
+						`),M=a($,"A",{class:!0,target:!0,href:!0,rel:!0});var fe=l(M);xt=o(fe,"Everreal Gmbh"),fe.forEach(n),At=o($,`
+						focused on digital solution for renting and selling in \u{1F1E9}\u{1F1EA} and \u{1F1E8}\u{1F1ED} soon will be in \u{1F1E6}\u{1F1F9}.`),$.forEach(n),Rt=v(V),h=a(V,"LI",{class:!0});var H=l(h);U=a(H,"I",{class:!0}),l(U).forEach(n),Lt=o(H,`
+						\u{1F3D7} Find more about my works on
+						`),_=a(H,"A",{class:!0,target:!0,href:!0,rel:!0});var Xt=l(_);Bt=o(Xt,`gitlab
+							`),Z=a(Xt,"I",{class:!0}),l(Z).forEach(n),Xt.forEach(n),Dt=o(H,`
+						and
+						`),w=a(H,"A",{class:!0,target:!0,href:!0,rel:!0});var Yt=l(w);Kt=o(Yt,`github
+							`),C=a(Yt,"I",{class:!0}),l(C).forEach(n),Yt.forEach(n),Mt=o(H,`
+						.`),H.forEach(n),Vt=v(V),g=a(V,"LI",{class:!0});var P=l(g);j=a(P,"I",{class:!0}),l(j).forEach(n),Ft=o(P,`
+						\u{1F4EC} Reach me on
+						`),k=a(P,"A",{class:!0,target:!0,href:!0,rel:!0});var $t=l(k);Ht=o($t,`linkedin
+							`),z=a($t,"I",{class:!0}),l(z).forEach(n),$t.forEach(n),Pt=o(P,`
+						or
+						`),E=a(P,"A",{class:!0,target:!0,href:!0,rel:!0});var te=l(E);St=o(te,`mail me
+							`),N=a(te,"I",{class:!0}),l(N).forEach(n),te.forEach(n),Gt=o(P,`
+						.`),P.forEach(n),qt=v(V),p=a(V,"LI",{class:!0});var S=l(p);Q=a(S,"I",{class:!0}),l(Q).forEach(n),Jt=o(S,`
+						\u{1FA82} Follow me on
+						`),I=a(S,"A",{class:!0,target:!0,href:!0,rel:!0});var ee=l(I);Ot=o(ee,`twitter
+							`),W=a(ee,"I",{class:!0}),l(W).forEach(n),ee.forEach(n),Tt=o(S,`
+						and
+						`),y=a(S,"A",{class:!0,target:!0,href:!0,rel:!0});var re=l(y);Ut=o(re,`500px
+							`),X=a(re,"I",{class:!0}),l(X).forEach(n),re.forEach(n),Zt=o(S,`
+						.`),S.forEach(n),V.forEach(n),ue.forEach(n),d.forEach(n),Ct=v(u),jt=a(u,"BR",{}),zt=v(u),Nt=a(u,"BR",{}),Qt=v(u),F=a(u,"P",{class:!0});var ae=l(F);Wt=o(ae,`crafted with
+			`),J=a(ae,"I",{class:!0,style:!0}),l(J).forEach(n),ae.forEach(n),u.forEach(n),tt.forEach(n),this.h()},h(){e(G,"class","sm:text-5xl font-bold mt-16 sm:mt-0 text-4xl"),e(A,"class","cursor-pointer underline underline-offset-2 decoration-1 decoration-indigo-500"),e(A,"target","_blank"),e(A,"href","https://en.wikipedia.org/wiki/Munich"),e(A,"rel","noreferrer"),e(R,"class","cursor-pointer underline underline-offset-2 decoration-1 decoration-indigo-500"),e(R,"target","_blank"),e(R,"href","https://en.wikipedia.org/wiki/Kerala"),e(R,"rel","noreferrer"),e(b,"class","text-slate-500 dark:text-slate-400"),e(L,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-black-500"),e(L,"target","_blank"),e(L,"href","https://deno.land/"),e(L,"rel","noreferrer"),e(B,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-red-500"),e(B,"target","_blank"),e(B,"href","https://www.rust-lang.org/"),e(B,"rel","noreferrer"),e(D,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-orange-300"),e(D,"target","_blank"),e(D,"href","https://g.co/kgs/4g13UG"),e(D,"rel","noreferrer"),e(T,"class","fas fa-angle-right"),e(M,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-indigo-900"),e(M,"target","_blank"),e(M,"href","https://www.everreal.co/"),e(M,"rel","noreferrer"),e(K,"class","mb-1"),e(U,"class","fas fa-angle-right"),e(Z,"class","fab fa-gitlab"),e(_,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-orange-500"),e(_,"target","_blank"),e(_,"href","https://gitlab.com/sachin-philip"),e(_,"rel","noreferrer"),e(C,"class","fab fa-github"),e(w,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-black-500"),e(w,"target","_blank"),e(w,"href","https://github.com/sachin-philip"),e(w,"rel","noreferrer"),e(h,"class","mb-1"),e(j,"class","fas fa-angle-right"),e(z,"class","fab fa-linkedin"),e(k,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-blue-600"),e(k,"target","_blank"),e(k,"href","https://www.linkedin.com/in/sachinphilip/"),e(k,"rel","noreferrer"),e(N,"class","fas fa-envelope"),e(E,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-indigo-500"),e(E,"target","_blank"),e(E,"href","mailto:me@sachinphilip.com"),e(E,"rel","noreferrer"),e(g,"class","mb-1"),e(Q,"class","fas fa-angle-right"),e(W,"class","fab fa-twitter-square"),e(I,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-blue-400"),e(I,"target","_blank"),e(I,"href","https://twitter.com/sachin_philip"),e(I,"rel","noreferrer"),e(X,"class","fab fa-500px"),e(y,"class","cursor-pointer underline underline-offset-2 decoration-2 decoration-grey-500"),e(y,"target","_blank"),e(y,"href","https://500px.com/p/sachin_philip"),e(y,"rel","noreferrer"),e(p,"class","mb-1"),e(f,"class","text-md"),e(q,"class","columns"),e(i,"class","text-slate-600 dark:text-slate-300 mt-2 text-lg font-light"),e(J,"class","fas fa-heart animate__animated animate__pulse animate__infinite infinite"),ve(J,"color","#f14668"),e(F,"class","italic"),e(c,"class","sm:container sm:mx-auto px-4"),e(x,"class","grid place-items-center sm:h-screen h-full")},m(Y,tt){be(Y,x,tt),t(x,c),t(c,G),t(G,et),t(c,rt),t(c,b),t(b,at),t(b,A),t(A,nt),t(b,lt),t(b,R),t(R,st),t(b,ot),t(c,it),t(c,ct),t(c,dt),t(c,i),t(i,ut),t(i,L),t(L,ft),t(i,ht),t(i,B),t(B,gt),t(i,pt),t(i,mt),t(i,vt),t(i,D),t(D,bt),t(i,_t),t(i,wt),t(i,kt),t(i,Et),t(i,It),t(i,q),t(q,f),t(f,K),t(K,T),t(K,yt),t(K,M),t(M,xt),t(K,At),t(f,Rt),t(f,h),t(h,U),t(h,Lt),t(h,_),t(_,Bt),t(_,Z),t(h,Dt),t(h,w),t(w,Kt),t(w,C),t(h,Mt),t(f,Vt),t(f,g),t(g,j),t(g,Ft),t(g,k),t(k,Ht),t(k,z),t(g,Pt),t(g,E),t(E,St),t(E,N),t(g,Gt),t(f,qt),t(f,p),t(p,Q),t(p,Jt),t(p,I),t(I,Ot),t(I,W),t(p,Tt),t(p,y),t(y,Ut),t(y,X),t(p,Zt),t(c,Ct),t(c,jt),t(c,zt),t(c,Nt),t(c,Qt),t(c,F),t(F,Wt),t(F,J)},p:ne,i:ne,o:ne,d(Y){Y&&n(x)}}}class ke extends ge{constructor(x){super(),pe(this,x,null,_e,me,{})}}export{ke as default};
